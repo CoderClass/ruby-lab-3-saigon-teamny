dbfe32debbae892ecd380c6f690e939a06a6b7f2
Eddie Kim <ekim125@gmail.com>
2016-10-26 20:23:08 +0700

    bundole exec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Use .alphabetical because it's a class method. Example: Product.alphabetical
+  # Use #long_name when #long_name is an instance method. Example: @product.long_name
+  describe ".alphabetical" do
+    it "returns [] when there are no products" do
+    	products = Product.all
+    	expect(products.empty?).to be true
+    	expect(products).to eq []
+    end
+
+    it "returns [a] when there is only one product a" do
+      # a = Product.create!(...)
+      products = Product.all
+      a=Product.create!(name: "fitted", description: "fitted cap", image: "blah", price_VND: "25000000", weight: "2")
+      expect(products.empty?).to eq false
+      expect(products).to eq [a]
+    end
+
+    it "returns [a,b,c] after I create 3 products c, b, a" do
+      # ...
+      products = Product.all
+      a=Product.create!(name: "fitted", description: "fitted cap", image: "blah", price_VND: "25000000", weight: "2")
+      b=Product.create!(name: "snapback", description: "snapback cap", image: "blah", price_VND: "35000000", weight: "3")
+      c=Product.create!(name: "mexico", description: "mexico cap", image: "blah", price_VND: "75000000", weight: "5")
+      expect(products.empty?).to eq false
+      expect(products).to eq [a,c,b]
+    end
+  end
 end
